ce069ded249c4c6d229b3cf5cb082b925fccea82
wand3 <godswillbaiyewu@gmail.com>
2023-08-25 23:22:57 -0400

    patients database model

---
+++ models/models.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Patient Model: create a SQLAlchemy model Patient
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class Patient(Base):
+    """Class Patient: Database table named patients
+    Attributes:
+    * id, integer primary key
+    * fname, non-nullable string
+    * lname, non-nullable string
+    * oname, nullable string
+    * address, non-nullable string
+    * email, non-nullable string
+    * phone, non-nullable integer
+    * history, 
+    * doctor, nullable string
+    """
+    __tablename__ = 'patients'
+
+    id = Column(Integer, primary_key=True)
+    fname = Column(String(100), nullable=False)
+    lname = Column(String(100), nullable=False)
+    oname = Column(String(100), nullable=True)
+    address = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True)
+    phone = Column(Integer, unique=True, nullable=False)
+#     establish database relationship
+#     history = Column
+    doctor = Column(String(100), nullable=True)
