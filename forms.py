40aff10edde53ba5bb2f9f6267ab1ff3cde20615
wand3 <godswillbaiyewu@gmail.com>
2023-10-25 20:19:17 -0400

    template inheritance and register route

--- forms.py
+++ forms.py
@@ -1,19 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextField
-from wtf.validators import DataRequired
+from wtforms import StringField, SubmitField, TextAreaField, TelField, IntegerField, EmailField
+from wtforms.validators import DataRequired
 
 
 class PatientRegForm(FlaskForm):
-    fname = StringField( 'fname', validators=[DataRequired()])
-    lname = StringField( 'lname', validators=[DataRequired()])
-    oname = StringField( 'oname', validators=[DataRequired()])
-    address = StringField( 'address', validators=[DataRequired()])
-    email = StringField( 'email', validators=[DataRequired()])
-    phone  = StringField( 'phone', validators=[DataRequired()])
-    mob = StringField( 'mob', validators=[DataRequired()])
-    yob = StringField( 'yob', validators=[DataRequired()])
-    gender = StringField( 'gender', validators=[DataRequired()])
-    bloodGroup = StringField( 'bloodGroup', validators=[DataRequired()])
-    genotype = StringField( 'genotype', validators=[DataRequired()])
-    history = TextField( 'history', validators=[DataRequired()])
-    doctor = StringField( 'doctor', validators=[DataRequired()])
+    fname = StringField( 'First Name', validators=[DataRequired()])
+    lname = StringField( 'Last Name', validators=[DataRequired()])
+    oname = StringField( 'Other Name(s)', validators=[DataRequired()])
+    address = StringField( 'Address', validators=[DataRequired()])
+    email = EmailField( 'Email', validators=[DataRequired()])
+    phone  = TelField( 'Phone Number', validators=[DataRequired()])
+    mob = IntegerField( 'Month', validators=[DataRequired()])
+    yob = IntegerField( 'Year', validators=[DataRequired()])
+    gender = StringField( 'Gender', validators=[DataRequired()])
+    bloodGroup = StringField( 'Blood Group', validators=[DataRequired()])
+    genotype = StringField( 'Genotype', validators=[DataRequired()])
+    history = TextAreaField( 'History', validators=[DataRequired()])
+    doctor = StringField( 'Doctor', validators=[DataRequired()])
--- webapp/views/pis.py
+++ webapp/views/pis.py
@@ -1,13 +1,27 @@
 
 from flask import Flask, render_template, url_for
 from webapp.views import app_views
+from forms import PatientRegForm
 
 
-# add patient route
+# index route
 @app_views.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    return render_template("index.html")
+    return render_template("register.html")
 
+# Patient Registeration route
+@app_views.route('/register', methods=['POST'], strict_slashes=False)
+def register():
+    form = PatientRegForm
+    def pre_validate(self, form):
+        """
+        Override if you need field-level validation. Runs before any other
+        validators.
+
+        :param form: The form the field belongs to.
+        """
+        pass
+    return render_template("register.html", form=form)
 
 # get patient route
 
