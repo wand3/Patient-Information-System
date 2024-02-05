d3b0b392c199e7693844c834e84fe227c3c2df8e
wand3 <godswillbaiyewu@gmail.com>
2024-02-04 22:30:38 -0500

    webapp environmental variables for dev and test

--- webapp/admin/admin.py
+++ webapp/admin/admin.py
@@ -9,6 +9,12 @@ from webapp.auth import has_role
 from config import db_session
 
 
+# assign roles to users
+# edit users and patients records
+# search database with filters of either users and patients
+# display count of entire database users and patients
+# display doctors and record officers actions and time
+
 @login_required
 @has_role('administrator')
 @admin_views.route('/base', methods=["GET", "POST"], strict_slashes=False)
@@ -17,3 +23,4 @@ def base():
     patients = db_session.query(Patient).all()
 
     return render_template('admin.html', users=users, patients=patients)
+
