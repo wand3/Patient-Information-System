6a34b48a445a9a83832162d70ef7644bdbce89fe
wand3 <godswillbaiyewu@gmail.com>
2024-02-08 19:41:32 -0500

    js import in templates

--- webapp/api/v1/admin/admin.py
+++ webapp/api/v1/admin/admin.py
@@ -1,7 +1,13 @@
 
-from flask import request, jsonify
+from flask import request, jsonify, redirect, url_for
 from webapp.api.v1 import api_views
+from models.user import User
+from config import db_session
 
-@api_views.route('/', methods=["GET"], strict_slashes=False)
-def delete_user_role():
-    return '<h1>check</h1>'
+
+@api_views.route('/admin_delete_user_role/<int: id>', methods=["DELETE"], strict_slashes=False)
+def admin_delete_user_role(id):
+    users = db_session.query(User).all()
+
+
+    return redirect(url_for('admin.base', users=users))
