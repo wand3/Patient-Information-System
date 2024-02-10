da578af64587078a2ecf36b9d05d47c8a8924042
wand3 <godswillbaiyewu@gmail.com>
2024-02-10 16:05:37 -0500

    delete user role successful and flash message set

--- webapp/api/v1/admin/admin.py
+++ webapp/api/v1/admin/admin.py
@@ -1,5 +1,5 @@
 
-from flask import request, jsonify, redirect, url_for, render_template
+from flask import request, jsonify, redirect, url_for, render_template, flash
 from webapp.api.v1 import api_views
 from models.user import User
 from config import db_session
@@ -13,20 +13,8 @@ def admin_delete_user_role(id):
     if delete_id:
         delete_id.delete_user_role(id=delete_id)
         db_session.commit()
-        return jsonify({'message': f'User {id} role deleted!'})
+        flash(f"User {delete_id.username}'s Role deleted")      
+        return redirect(url_for("admin.base"))
     else: 
         db_session.roll_back()
         return jsonify({'Message': f'User {id} not found'}), 404
-    
-
-    # db_session.commit()
-
-
-
-    # for u in user:
-    #     if id == user.id:
-    #         return jsonify({'message': f'User {id} role deleted!'})
-
-    #     else: 
-    #         return jsonify({'Message': f'User {id} not found'}), 404
-        
